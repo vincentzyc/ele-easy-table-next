5850d574e69313e2062b1e897326a1cd6fdb223e
vincentzyc <vincentzyc@qq.com>
2021-12-10 09:48:49 +0800

    init: init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { Search, Edit, Check, Message, Star, Delete } from '@element-plus/icons'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
@@ -31,6 +31,42 @@ const count = ref(0)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button :icon="Search" circle></el-button>
+    <el-button type="primary" :icon="Edit" circle></el-button>
+    <el-button type="success" :icon="Check" circle></el-button>
+    <el-button type="info" :icon="Message" circle></el-button>
+    <el-button type="warning" :icon="Star" circle></el-button>
+    <el-button type="danger" :icon="Delete" circle></el-button>
+  </el-row>
 </template>
 
 <style scoped>
