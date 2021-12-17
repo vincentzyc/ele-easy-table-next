83697b7bb1c281549dfb2d689f0da9622400bdf7
vincentzyc <vincentzyc@qq.com>
2021-12-17 09:15:29 +0800

    ⚡ perf: 样式优化

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,6 @@ import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 <template>
   <el-config-provider :locale="zhCn">
-    <EleEasyTableDemo/>
+    <EleEasyTableDemo />
   </el-config-provider>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
