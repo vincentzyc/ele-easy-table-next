43dcfc9cd9bdb8249a4b6ad564bd14eb9b4a97b4
vincentzyc <vincentzyc@qq.com>
2021-12-17 09:25:39 +0800

    ✨ feat: EleEasyTable

--- src/components/EleEasyTable.vue
+++ src/components/EleEasyTable.vue
@@ -1,5 +1,86 @@
 <template>
   <div class="clearfix">
+    <el-form
+      :class="form.class"
+      :inline="true"
+      :model="formData"
+      :style="form.style"
+      class="relative"
+      size="small"
+      v-bind="form.config"
+      v-if="Object.keys(form).length > 0"
+    >
+      <span :key="key" v-for="(item,key) in form.list" v-show="showFormItem(item, key)">
+        <el-form-item :label="item.label ? item.label + '：' : ''" v-bind="item.formConfig" v-if="item.type !== 'slot'">
+          <el-date-picker
+            :style="item.style || form.formItemStyle"
+            @change="getDate(item)"
+            end-placeholder="结束日期"
+            size="small"
+            start-placeholder="开始日期"
+            type="daterange"
+            v-bind="item.config"
+            v-if="item.type === 'datePicker'"
+            v-model="formData[item.key]"
+            value-format="yyyy-MM-dd"
+          ></el-date-picker>
+          <el-input
+            :placeholder="item.placeholder ? item.placeholder : '请输入' + item.label"
+            :style="item.style || form.formItemStyle"
+            @keyup.enter.native="$emit('get-list')"
+            size="small"
+            v-bind="item.config"
+            v-if="item.type === 'input'"
+            v-model="formData[item.key]"
+          ></el-input>
+          <el-select
+            :placeholder="item.placeholder ? item.placeholder : '请选择' + item.label"
+            :style="item.style || form.formItemStyle"
+            size="small"
+            v-bind="item.config"
+            v-if="item.type === 'select'"
+            v-model="formData[item.key]"
+          >
+            <el-option
+              :key="typeof (option.value) !== 'undefined' ? option.value : option"
+              :label="typeof (option.label) !== 'undefined' ? option.label : option"
+              :value="typeof (option.value) !== 'undefined' ? option.value : option"
+              v-for="option in item.options"
+            ></el-option>
+          </el-select>
+          <el-button
+            :disabled="item.disabled ? item.disabled() : false"
+            :style="item.style"
+            @click="item.handleClick(item, key)"
+            size="small"
+            type="primary"
+            v-bind="item.config"
+            v-if="item.type === 'button'"
+          >{{ typeof item.text === 'function' ? item.text() : item.text }}</el-button>
+        </el-form-item>
+        <slot :name="item.slot" v-else></slot>
+      </span>
+      <div class="text-center" v-if="showFold">
+        <el-button @click="handleExpand()" type="text">
+          {{ isExpand ? '收起' : '展开' }}
+          <svg
+            :class="{ 'arrow-expand': isExpand }"
+            class="arrow-icon"
+            fill="#409EFF"
+            height="12px"
+            viewBox="0 0 48 48"
+            width="12px"
+          >
+            <g fill-rule="evenodd">
+              <path
+                d="M24 21.91l10.586-10.586a2 2 0 0 1 2.828 2.828l-12 12a2 2 0 0 1-2.828 0l-12-12a2 2 0 0 1 2.828-2.828L24 21.91zm-10.586 1.414L24 33.91l10.586-10.586a2 2 0 0 1 2.828 2.828l-12 12a2 2 0 0 1-2.828 0l-12-12a2 2 0 0 1 2.828-2.828z"
+                fill-rule="nonzero"
+              />
+            </g>
+          </svg>
+        </el-button>
+      </div>
+    </el-form>
     <el-table
       :data="table.list"
       border
@@ -72,7 +153,7 @@
         :layout="typeof (pagination) === 'object' ? pagination.layout : 'total, sizes, prev, pager, next, jumper'"
         :page-size="formData.pageSize || 10"
         :page-sizes="typeof (pagination) === 'object' ? pagination.pageSizes : [10, 20, 50, 100]"
-        :total="formData.totalCount || 0"
+        :total="11"
         @current-change="handleCurrentChange"
         @size-change="handleSizeChange"
         v-bind="typeof (pagination) === 'object' ? pagination : {}"
@@ -82,7 +163,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
+import { computed, defineComponent, ref, watch } from 'vue'
 
 export default defineComponent({
   name: "EleEasyTable"
@@ -93,6 +174,12 @@ export default defineComponent({
 
 const props = defineProps({
   formData: {
+    type: Object,
+    default() {
+      return {}
+    }
+  },
+  form: {
     type: Object,
     default() {
       return {}
@@ -112,8 +199,9 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(['update:formData', 'get-list'])
+const emit = defineEmits(['update:formData', 'get-list', 'handle-expand'])
 
+const isExpand = ref(false)
 const tableIndex = ref(0)
 
 watch(() => props.table.list, () => {
@@ -128,6 +216,24 @@ watch(() => props.table.list, () => {
   }
 }, { immediate: true })
 
+const showFold = computed(() => {
+  if (typeof props.form.foldNum !== 'number' || props.form.foldNum <= 0) return false
+  let num = props.form.list.filter((v: Record<string, any>) => !(v.type === 'button' || v.fold === false)).length
+  return num > props.form.foldNum - 1
+})
+
+function handleExpand() {
+  isExpand.value = !isExpand.value;
+  emit('handle-expand', isExpand.value);
+}
+function showFormItem(item: Record<string, any>, key: number) {
+  if (typeof props.form.foldNum !== 'number' || props.form.foldNum <= 0 || item.type === 'button' || item.fold === false || isExpand.value) return true
+  return key <= props.form.foldNum - 1
+}
+function getDate(item: Record<string, any>) {
+  props.formData[item.startKey] = props.formData[item.key] ? props.formData[item.key][0] : "";
+  props.formData[item.endKey] = props.formData[item.key] ? props.formData[item.key][1] : "";
+}
 function handleSizeChange(val: number) {
   props.formData.pageIndex = 1
   props.formData.pageSize = val
