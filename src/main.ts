4a70b51ddeeb1028a57145a766037a6a30a9230b
vincentzyc <vincentzyc@qq.com>
2021-12-20 08:57:02 +0800

    🎉 init: init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
-
 import EleEasyTable from './index'
 
-createApp(App).use(EleEasyTable).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.use(EleEasyTable)
+
+app.mount('#app')
