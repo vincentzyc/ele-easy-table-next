1236c9f172f793d35d071576bd975ddb84426b7c
vincentzyc <vincentzyc@qq.com>
2021-12-22 09:26:26 +0800

    🎉 init: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-const path = require('path')
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import AutoImport from 'unplugin-auto-import/vite'
