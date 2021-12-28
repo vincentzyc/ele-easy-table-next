babfc3c32f94eaebd32fdd0dee01223f59c53a47
vincentzyc <vincentzyc@qq.com>
2021-12-28 09:53:07 +0800

    🎉 init: init

--- src/components/CustomColumn/type.ts
+++ src/components/CustomColumn/type.ts
@@ -6,10 +6,11 @@ export interface TypeTextBtnItem {
 export interface TypeColumnsItem {
   key: string,
   label: string,
-  header: string,
-  type: string,
-  slot: string,
-  disabledCustom: boolean,
-  textBtn: TypeTextBtnItem[],
-  format: () => void
+  header?: string,
+  type?: string,
+  slot?: string,
+  disabledCustom?: boolean,
+  textBtn?: TypeTextBtnItem[],
+  config?: Record<string, any>,
+  format?: () => void
 }
