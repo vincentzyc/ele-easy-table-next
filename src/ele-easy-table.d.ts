744f0e4ae4de79c24363b23ff2bbcdc30cdc5204
vincentzyc <vincentzyc@qq.com>
2022-01-04 11:27:51 +0800

    🎉 init: init

--- src/ele-easy-table.d.ts
+++ src/ele-easy-table.d.ts
@@ -1,79 +1,4 @@
-// declare const draggableComponent: import("vue").DefineComponent<{
-//     list: {
-//         type: ArrayConstructor;
-//         required: boolean;
-//         default: any;
-//     };
-//     modelValue: {
-//         type: ArrayConstructor;
-//         required: boolean;
-//         default: any;
-//     };
-//     itemKey: {
-//         type: (FunctionConstructor | StringConstructor)[];
-//         required: boolean;
-//     };
-//     clone: {
-//         type: FunctionConstructor;
-//         default: (original: any) => any;
-//     };
-//     tag: {
-//         type: StringConstructor;
-//         default: string;
-//     };
-//     move: {
-//         type: FunctionConstructor;
-//         default: any;
-//     };
-//     componentData: {
-//         type: ObjectConstructor;
-//         required: boolean;
-//         default: any;
-//     };
-// }, unknown, {
-//     error: boolean;
-// }, {
-//     realList(): any;
-//     getKey(): any;
-// }, {
-//     getUnderlyingVm(domElement: any): any;
-//     getUnderlyingPotencialDraggableComponent(htmElement: any): any;
-//     emitChanges(evt: any): void;
-//     alterList(onList: any): void;
-//     spliceList(): void;
-//     updatePosition(oldIndex: any, newIndex: any): void;
-//     getRelatedContextFromMoveEvent({ to, related }: {
-//         to: any;
-//         related: any;
-//     }): any;
-//     getVmIndexFromDomIndex(domIndex: any): any;
-//     onDragStart(evt: any): void;
-//     onDragAdd(evt: any): void;
-//     onDragRemove(evt: any): void;
-//     onDragUpdate(evt: any): void;
-//     computeFutureIndex(relatedContext: any, evt: any): any;
-//     onDragMove(evt: any, originalEvent: any): any;
-//     onDragEnd(): void;
-// }, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, any[], any, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{
-//     move: Function;
-//     tag: string;
-//     clone: Function;
-//     list: unknown[];
-//     modelValue: unknown[];
-//     componentData: Record<string, any>;
-// } & {
-//     itemKey?: string | Function;
-// }>, {
-//     move: Function;
-//     tag: string;
-//     clone: Function;
-//     list: unknown[];
-//     modelValue: unknown[];
-//     componentData: Record<string, any>;
-// }>;
-// export default draggableComponent;
-
-declare const draggableComponent: import("vue").DefineComponent<{
+export declare const EleEasyTable: import("vue").DefineComponent<{
   formData: {
     type: ObjectConstructor;
     required: boolean;
@@ -96,3 +21,8 @@ declare const draggableComponent: import("vue").DefineComponent<{
     default: true
   }
 }>
+
+declare const _default: {
+  install: (app: any) => void;
+};
+export default _default;
