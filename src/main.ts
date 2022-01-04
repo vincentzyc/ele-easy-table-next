5d37af333ca91be8d9c41b83fda494ab8ae82c62
vincentzyc <vincentzyc@qq.com>
2022-01-04 14:25:41 +0800

    🎈 release: v1.0.5

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-import EleEasyTable from '../dist/ele-easy-table-next.es.js'
+import EleEasyTable from './index'
 
 const app = createApp(App)
 
