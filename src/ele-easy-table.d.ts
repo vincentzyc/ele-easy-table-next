97a863e420b00cb334f1db67831a8409ba8d3302
vincentzyc <vincentzyc@qq.com>
2022-01-05 11:05:56 +0800

    🐳 chore: add CustomColumn types

--- src/ele-easy-table.d.ts
+++ src/ele-easy-table.d.ts
@@ -22,6 +22,31 @@ export declare const EleEasyTable: import("vue").DefineComponent<{
   }
 }>
 
+export interface TypeTextBtnItem {
+  text: string,
+  funcConfig?: (row: any) => any
+  handleClick: (row: any, scope: any) => any
+}
+export interface TypeColumnsItem {
+  key: string,
+  label: string,
+  header?: string,
+  type?: string,
+  slot?: string,
+  disabledCustom?: boolean,
+  textBtn?: TypeTextBtnItem[],
+  config?: Record<string, any>,
+  format?: (row: any) => any
+}
+
+export declare const CustomColumn: import("vue").DefineComponent<{
+  localName: string,
+  show?: boolean,
+  baseColumns?: TypeColumnsItem[],
+  columns?: TypeColumnsItem[],
+  defaultColumns?: string[]
+}>
+
 declare const _default: {
   install: (app: any) => void;
 };
