47da2466682045a4e6a706b9669a4e2d13734666
vincentzyc <vincentzyc@qq.com>
2022-05-20 14:16:20 +0800

    🎉 init: vuecli init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import EleEasyTableDemo from './components/EleEasyTableDemo.vue'
-import { ElConfigProvider } from 'element-plus'
-import zhCn from 'element-plus/lib/locale/lang/zh-cn'
-</script>
-
 <template>
   <el-config-provider :locale="zhCn">
     <EleEasyTableDemo />
   </el-config-provider>
 </template>
+
+<script setup lang="ts">
+import EleEasyTableDemo from './components/EleEasyTableDemo.vue'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+</script>
