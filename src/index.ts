ce9ca09dddb48637f4e682c8e8d529f2d5867aed
vincentzyc <vincentzyc@qq.com>
2022-05-24 10:10:22 +0800

    🎉 init: vuecli init

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import EleEasyTable from './components/EleEasyTable.vue'
 import CustomColumn from './components/CustomColumn/index.vue'
+import { App } from 'vue'
 
 export default {
-  install: (app: any) => {
+  install: (app: App) => {
     app.component('EleEasyTable', EleEasyTable)
     app.component('CustomColumn', CustomColumn)
   }
