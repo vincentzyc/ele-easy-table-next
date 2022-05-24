138d18437b00462005de5fdf6b1a90a0575dec08
vincentzyc <vincentzyc@qq.com>
2022-05-24 10:10:06 +0800

    🎉 init: vuecli init

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
-		extract: false
+    extract: false
   },
   outputDir: "docs",
-	productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    // 以下库使用外部资源，不会被打包
+    externals: process.env.NODE_ENV === 'production' ? {
+      "vue": "Vue",
+      "vuedraggable": "vuedraggable",
+      "element-plus": "ElementPlus"
+    } : {}
+  },
 })
