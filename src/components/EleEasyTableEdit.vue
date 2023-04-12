3d57148e571bfbc88a07b57c5256caa5a4bcd0b9
vincentzyc <vincentzyc@qq.com>
2023-04-12 15:40:14 +0800

    ✨feat: add EleEasyTableEdit

---
+++ src/components/EleEasyTableEdit.vue
@@ -0,0 +1,47 @@
+<template>
+  <span v-bind="config" v-if="!isEdit" v-on="$attrs">{{ column[prop] }}</span>
+  <el-select
+    filterable
+    placeholder="请选择"
+    size="small"
+    v-bind="config"
+    v-else-if="options"
+    v-model="column[prop]"
+    v-on="$attrs"
+  >
+    <el-option :key="item.code" :label="item.label" :value="item.label" v-for="item in options"></el-option>
+  </el-select>
+  <el-input
+    :class="prop + column.id"
+    placeholder="请输入内容"
+    size="small"
+    v-bind="config"
+    v-else
+    v-model.trim="column[prop]"
+    v-on="$attrs"
+  ></el-input>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EleEasyTableEdit',
+});
+</script>
+
+<script lang="ts" setup>
+interface TypeProps {
+  column: Record<string, any>;
+  prop: string;
+  isEdit?: boolean;
+  options?: any[];
+  config?: Record<string, any>;
+}
+
+withDefaults(defineProps<TypeProps>(), {
+  isEdit: false,
+  options: () => [],
+  config: () => ({}),
+});
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import EleEasyTable from './components/EleEasyTable.vue'
-import CustomColumn from './components/CustomColumn/index.vue'
-import { App } from 'vue'
+import EleEasyTable from './components/EleEasyTable.vue';
+import CustomColumn from './components/CustomColumn/index.vue';
+import EleEasyTableEdit from './components/CustomColumn/index.vue';
+import { App } from 'vue';
 
 export default {
   install: (app: App) => {
-    app.component('EleEasyTable', EleEasyTable)
-    app.component('CustomColumn', CustomColumn)
-  }
-}
+    app.component('EleEasyTable', EleEasyTable);
+    app.component('CustomColumn', CustomColumn);
+    app.component('EleEasyTableEdit', EleEasyTableEdit);
+  },
+};
